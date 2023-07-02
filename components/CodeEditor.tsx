43f2dfdeb88de3d860d99a3c3a05528ebb040ee7
Louis Arge <louislva@gmail.com>
2023-07-02 10:44:18 -0700

    Gaze

--- components/CodeEditor.tsx
+++ components/CodeEditor.tsx
@@ -1,26 +1,86 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import hljs from "highlight.js";
 
-const CodeEditor = ({
-  value,
-  onChange,
-}: {
+const CodeBlock = (props: {
+  value: string;
+  gazeY: number;
+  onGaze: (whereIsGaze: "over" | "under" | "on") => void;
+  highlightLevel: 0 | 1 | 2;
+}) => {
+  const { value, gazeY, onGaze, highlightLevel } = props;
+  const ref = useRef<HTMLPreElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      const startY = ref.current.getBoundingClientRect().top;
+      const endY = ref.current.getBoundingClientRect().bottom;
+
+      console.log(startY, endY, "res:", gazeY > startY && gazeY < endY);
+
+      onGaze(gazeY < startY ? "over" : gazeY > endY ? "under" : "on");
+    }
+  }, [gazeY]);
+
+  return (
+    <div
+      className={
+        "font-mono " +
+        (highlightLevel === 2
+          ? "bg-green-500/20"
+          : highlightLevel === 1
+          ? "bg-green-500/10"
+          : "bg-transparent")
+      }
+    >
+      <code ref={ref}>
+        <pre>{value}</pre>
+      </code>
+    </div>
+  );
+};
+const CodeEditor = (props: {
+  gazeY: number;
   value: string;
   onChange: (value: string) => void;
 }) => {
+  const { gazeY, value, onChange } = props;
+
   useEffect(() => {
     setInterval(() => {
       hljs.highlightAll();
     }, 500);
   }, []);
 
+  const [highlightIndex, setHighlightIndex] = useState<number | null>(null);
+  const paragraphs = value.split("\n\n");
   return (
     <>
-      {value.split("\n\n").map((paragraph) => {
+      {paragraphs.map((paragraph, index) => {
         return (
-          <code className="">
-            <pre>{paragraph}</pre>
-          </code>
+          <CodeBlock
+            value={paragraph}
+            gazeY={gazeY}
+            onGaze={(whereIsGaze) => {
+              if (whereIsGaze === "on") {
+                setHighlightIndex(index);
+              }
+              if (index === paragraphs.length - 1 && whereIsGaze === "under") {
+                // if we're on the last paragraph and the gaze is still further down, no highlight
+                setHighlightIndex(index + 1);
+              }
+              if (index === 0 && whereIsGaze === "over") {
+                // if we're on the first paragraph and the gaze is still further up, no highlight
+                setHighlightIndex(-1);
+              }
+            }}
+            highlightLevel={
+              highlightIndex === index
+                ? 2
+                : Math.abs((highlightIndex || -1000) - index) === 1
+                ? 1
+                : 0
+            }
+          />
         );
       })}
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
 import CodeEditor from "@/components/CodeEditor";
+import Head from "next/head";
+import { useEffect, useRef, useState } from "react";
+import hljs from "highlight.js";
 
 const exampleDoc = `const input = "";
 
@@ -12,10 +15,58 @@ const output = input
 
 console.log({ output });`;
 
+// Tell typescript that webgazer actually exists as a global variable
+declare var webgazer: any;
+
 export default function Home() {
+  const [gazeY, setGazeY] = useState(0);
+  const gazeYRef = useRef(0);
+  gazeYRef.current = gazeY;
+  const lastGazeYUpdate = useRef(0);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      webgazer.begin();
+      webgazer.applyKalmanFilter(true);
+      webgazer.setGazeListener((data: any, elapsedTime: number) => {
+        // console.log({ data, elapsedTime });
+        if (data) {
+          const y = Math.min(Math.max(data.y, 0), window.innerHeight);
+
+          if (
+            Math.abs(gazeYRef.current - y) > 8 &&
+            Date.now() - lastGazeYUpdate.current > 100
+          ) {
+            setGazeY(y);
+            lastGazeYUpdate.current = Date.now();
+          }
+        }
+      });
+    }
+  }, []);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CodeEditor value={exampleDoc} onChange={(str) => {}} />
-    </div>
+    <>
+      <Head>
+        {/* required before everything else loads */}
+        <script src="/webgazer.js" type="text/javascript" />
+      </Head>
+
+      <div className="flex w-full min-h-screen flex-row bg-zinc-700">
+        <div className="w-80 flex flex-col">
+          <div className="h-60 bg-black w-full"></div>
+        </div>
+        <div className="flex-1 p-4 flex flex-col">
+          <div className="p-2 rounded-md bg-zinc-800 text-white flex-1">
+            {/* <Callibration /> */}
+            <CodeEditor
+              gazeY={gazeY}
+              value={exampleDoc}
+              onChange={(str) => {}}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
